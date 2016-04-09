389f5138767768dc5e22666f0d96d66605f7ce0d
Benson Kamau <bkamau@abo.fi>
2016-04-09 20:29:02 +0300

    Update Defaults.java

--- app/src/main/java/fi/benson/fleaapp/defaults/Defaults.java
+++ app/src/main/java/fi/benson/fleaapp/defaults/Defaults.java
@@ -1,8 +1,8 @@
 package fi.benson.fleaapp.defaults;
 
 public class Defaults {
-    public static final String APPLICATION_ID = "CF641B47-1C16-3C9A-FFBE-2BEF7F447500";
-    public static final String SECRET_KEY = "FC632200-2B5F-33BE-FFE4-7AFA02BD8C00";
+    public static final String APPLICATION_ID = "";
+    public static final String SECRET_KEY = "";
     public static final String VERSION = "v1";
     public static final String SERVER_URL = "https://api.backendless.com";
     public static final int RESULT_OK = 101;
@@ -12,6 +12,6 @@ public class Defaults {
     public static final int REQUEST_IMAGE_CAPTURE = 1;
     public static final int PICK_IMAGE = 50;
     public static final int CAPTURE_IMAGE = 51;
-    public static final String IMAGE_DIRECTORY_NAME = "My Camera";
+    public static final String IMAGE_DIRECTORY_NAME = "";
 
 }
